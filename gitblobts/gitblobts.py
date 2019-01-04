89fb9928f42bd6374d4b59299553dc88e99316a0
Ouroboros Chrysopoeia <impredicative>
2019-01-03 22:54:10 -0500

    Make various fixes

--- gitblobts/gitblobts.py
+++ gitblobts/gitblobts.py
@@ -1,4 +1,5 @@
 import calendar
+import itertools
 import pathlib
 import time
 import typing
@@ -47,9 +48,6 @@ class Store:
     class TimeUnhandledType(TimeError):
         pass
 
-    class TimesNotUnique(TimeError):
-        pass
-
     def __init__(self, path: Union[str, pathlib.Path]):
         self._path = pathlib.Path(path)
         self._repo = git.Repo(self._path)  # Can raise git.exc.NoSuchPathError or git.exc.InvalidGitRepositoryError.
@@ -82,28 +80,27 @@ class Store:
 
     def _push_repo(self) -> None:
         self._repo.index.commit('')
-        self._repo.remote().push()  # TODO: Pull and retry if there is a merge conflict.
+        self._repo.remote().push()  # TODO: Pull and retry push to handle a merge conflict.
 
-    def _standardize_time_to_ns(self, time_utc: Union[None, str, float, time.struct_time]) -> int:
+    def _standardize_time_to_ns(self, time_utc: Union[None, float, time.struct_time, str]) -> int:
         def _convert_seconds_to_ns(seconds: Union[int, float]) -> int:
             return int(round(seconds * int(1e9)))
         if time_utc is None:
             return time.time_ns()
-        elif isinstance(str, time_utc):
-            return 'CONVERT SLANG UTC TIME'  # TODO: Convert slang UTC time.
         elif isinstance(float, time_utc):
             return _convert_seconds_to_ns(time_utc)
         elif isinstance(time_utc, time.struct_time):
             if time_utc.tm_zone != 'UTC':
                 raise self.TimeNotUTC(f"Provided timezone must be UTC, but it's {time_utc.tm_zone}.")
             return _convert_seconds_to_ns(calendar.timegm(time_utc))
+        elif isinstance(str, time_utc):
+            return 'CONVERT SLANG UTC TIME'  # TODO: Convert slang UTC time.
         else:
             annotation = typing.get_type_hints(self.writeblob)['time_utc']
             raise self.TimeUnhandledType(f'Provided time is of an unhandled type "{type(time_utc)}. '
                                          f'It must be conform to {annotation}.')
 
-    def writeblob(self, blob: bytes, time_utc: Optional[Union[float, time.struct_time]] = None,
-                  sync_repo: Optional[bool] = True) -> None:
+    def writeblob(self, blob: bytes, time_utc: Optional[float] = None, sync_repo: Optional[bool] = True) -> int:
         repo = self._repo
         time_utc_ns = self._standardize_time_to_ns(time_utc)
         path = self._path / str(time_utc_ns)
@@ -114,22 +111,23 @@ class Store:
         if sync_repo:
             self._push_repo()
         assert blob == path.read_bytes()
+        return time_utc_ns
 
-    def writeblobs(self, blobs: List[bytes], times_utc: Optional[List[Union[float, time.struct_time]]] = None) -> None:
-        repo = self._repo
+    def writeblobs(self, blobs: Iterable[bytes], times_utc: Optional[Iterable[float]] = None) -> List[int]:
         self._pull_repo()  # TODO: Consider pull only if there is a merge conflict.
-        if len(times_utc) != len(set(times_utc)):
-            raise self.TimesNotUnique("Provided times must be unique as they're used as filenames.")
-        for blob, time_utc in zip(blobs, times_utc):
-            self.writeblob(blob, times_utc, sync_repo=False)
-        repo.index.commit('')
-        repo.remote().push()  # TODO: Pull and retry if there is a merge conflict.
+        if times_utc is None:
+            times_utc = []
+        times_utc_ns = [self.writeblob(blob, time_utc, sync_repo=False) for blob, time_utc in
+                        itertools.zip_longest(blobs, times_utc)]
+        self._push_repo()
+        return times_utc_ns
 
-    def readblobs(self, start_utc: Union[str, float, time.struct_time],
-                  end_utc: Optional[Union[str, float, time.struct_time]] = None) -> Iterable[Blob]:
+    def readblobs(self, start_utc: Optional[Union[float, time.struct_time, str]] = None,
+                  end_utc: Optional[Union[float, time.struct_time, str]] = None) -> Iterable[Blob]:
+        # TODO: Return blobs in ascending or descending temporal order per order of start_utc and end_utc.
         self._pull_repo()
-        start_utc = self._standardize_time_to_ns(start_utc)
-        end_utc = self._standardize_time_to_ns(end_utc)
+        start_utc = self._standardize_time_to_ns(start_utc) if start_utc is not None else 0
+        end_utc = self._standardize_time_to_ns(end_utc) if end_utc is not None else float('inf')
         for path in self._path.iterdir():
             if path.is_file():
                 time_utc_ns = int(path.name)
