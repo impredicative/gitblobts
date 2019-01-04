44d558ecf88310b9b9ac4708d191428b35955b7e
Ouroboros Chrysopoeia <impredicative>
2019-01-03 19:22:15 -0500

    Implement Store.readblobs

--- README.md
+++ README.md
@@ -5,11 +5,10 @@ It stores a blob as a new file in a preexisting local git repo.
 It then commits and pushes the change.
 The file is assigned a nanosecond UTC timestamp as its name.
 The package allows subsequent retrieval of the blobs by a timestamp range.
+Collaborative use of the same remote repo is supported.
 
 The philosophy behind the package is that data must automatically be backed up elsewhere to guard against local data
 loss, while also remaining available locally.
-
-The package currently does not support any multi-user use.
 
 ## Installation
 Using Python 3.7+, run `pip install gitblobts`.
@@ -36,7 +35,8 @@ blobs = list(store.readblobs(start='1 week ago', end='now'))
 ```
 
 ## To do
+* Perform compression.
+* Organize blobs into directory structure: YYYY/MM/DD/HH
 * Support batching new blobs before commit+push.
-* Support compression.
 * Support encryption.
 * Support asyncio or avoiding waiting for commit+push.
--- gitblobts/gitblobts.py
+++ gitblobts/gitblobts.py
@@ -2,13 +2,13 @@ import calendar
 import pathlib
 import time
 import typing
-from typing import Iterable, NamedTuple, Optional, Union
+from typing import Iterable, List, NamedTuple, Optional, Union
 
 import git
 
 
 class Blob(NamedTuple):
-    timestamp: int
+    time_utc_ns: int
     blob: bytes
 
 
@@ -47,13 +47,14 @@ class Store:
     class TimeUnhandledType(TimeError):
         pass
 
-    def __init__(self, path: Union[str, pathlib.Path], multiuser: bool = False):
+    class TimesNotUnique(TimeError):
+        pass
+
+    def __init__(self, path: Union[str, pathlib.Path]):
         self._path = pathlib.Path(path)
-        self.repo = git.Repo(self._path)  # Can raise git.exc.NoSuchPathError or git.exc.InvalidGitRepositoryError.
+        self._repo = git.Repo(self._path)  # Can raise git.exc.NoSuchPathError or git.exc.InvalidGitRepositoryError.
         self._check_repo()
-        self._multiuser = multiuser
-        if self._multiuser:
-            self.repo.remote().pull()
+        self._pull_repo()
 
     def _check_repo(self) -> None:
         repo = self.repo
@@ -70,14 +71,26 @@ class Store:
             raise self.RepoRemoteNotAdded('Repository must have a remote.')
         if not repo.remote().exists():
             raise self.RepoRemoteNotExist('Repository remote must exist.')
-        # if not self.repo.remote().name == 'origin':
+        # if not self._repo.remote().name == 'origin':
         #     raise self.RemoteRepoError('Repository remote name must be "origin".')
 
-    def _standardize_time_to_ns(self, time_utc: Optional[Union[float, time.struct_time]] = None) -> int:
+    def _pull_repo(self) -> None:
+        try:
+            self._repo.remote().pull()
+        except git.exc.GitCommandError:  # Could be due to no push yet.
+            pass
+
+    def _push_repo(self) -> None:
+        self._repo.index.commit('')
+        self._repo.remote().push()  # TODO: Pull and retry if there is a merge conflict.
+
+    def _standardize_time_to_ns(self, time_utc: Union[None, str, float, time.struct_time]) -> int:
         def _convert_seconds_to_ns(seconds: Union[int, float]) -> int:
-            return int(round(seconds * 1e9))
+            return int(round(seconds * int(1e9)))
         if time_utc is None:
             return time.time_ns()
+        elif isinstance(str, time_utc):
+            return 'CONVERT SLANG UTC TIME'  # TODO: Convert slang UTC time.
         elif isinstance(float, time_utc):
             return _convert_seconds_to_ns(time_utc)
         elif isinstance(time_utc, time.struct_time):
@@ -89,16 +102,36 @@ class Store:
             raise self.TimeUnhandledType(f'Provided time is of an unhandled type "{type(time_utc)}. '
                                          f'It must be conform to {annotation}.')
 
-    def writeblob(self, blob: bytes, time_utc: Optional[Union[float, time.struct_time]] = None) -> None:
+    def writeblob(self, blob: bytes, time_utc: Optional[Union[float, time.struct_time]] = None,
+                  sync_repo: Optional[bool] = True) -> None:
+        repo = self._repo
         time_utc_ns = self._standardize_time_to_ns(time_utc)
         path = self._path / str(time_utc_ns)
-        if self._multiuser:
-            self.repo.remote().pull()
+        if sync_repo:
+            self._pull_repo()  # TODO: Consider pull only if there is a merge conflict.
         path.write_bytes(blob)
+        repo.index.add([path])
+        if sync_repo:
+            self._push_repo()
         assert blob == path.read_bytes()
-        self.repo.index.add(path)
-        self.repo.index.commit('')
-        self.repo.remote().push()
 
-    def readblobs(self, start, end) -> Iterable[Blob]:
-        pass
+    def writeblobs(self, blobs: List[bytes], times_utc: Optional[List[Union[float, time.struct_time]]] = None) -> None:
+        repo = self._repo
+        self._pull_repo()  # TODO: Consider pull only if there is a merge conflict.
+        if len(times_utc) != len(set(times_utc)):
+            raise self.TimesNotUnique("Provided times must be unique as they're used as filenames.")
+        for blob, time_utc in zip(blobs, times_utc):
+            self.writeblob(blob, times_utc, sync_repo=False)
+        repo.index.commit('')
+        repo.remote().push()  # TODO: Pull and retry if there is a merge conflict.
+
+    def readblobs(self, start_utc: Union[str, float, time.struct_time],
+                  end_utc: Optional[Union[str, float, time.struct_time]] = None) -> Iterable[Blob]:
+        self._pull_repo()
+        start_utc = self._standardize_time_to_ns(start_utc)
+        end_utc = self._standardize_time_to_ns(end_utc)
+        for path in self._path.iterdir():
+            if path.is_file():
+                time_utc_ns = int(path.name)
+                if start_utc <= time_utc_ns <= end_utc:
+                    yield Blob(time_utc_ns, path.read_bytes())
