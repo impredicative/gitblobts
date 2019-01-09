fd6801e0e7c2b58cbd105fd5661f491548915284
Ouroboros Chrysopoeia <impredicative>
2019-01-09 18:12:31 -0500

    Update usage example

--- gitblobts/__main__.py
+++ gitblobts/__main__.py
@@ -4,6 +4,8 @@ from gitblobts.store import Store
 configure_logging()
 
 if __name__ == '__main__':
-    store = Store('/home/devuser/Documents/blobdumptest', compression='gzip')
-    print(store.addblobs([str(i+5).encode()*111 for i in range(2)], ['now', 'now']))
-    print(next(store.getblobs('now', 'yesterday', pull=0), None))
+    import time
+    store = Store('/home/devuser/Documents/blobdumptest', compression=None or 'bz2',
+                  key=None or b'JVGmuw3wRntCc7dcQHJ5q1noUs62ydR0Nw8HpyllKn8=')
+    print(store.addblobs([str(time.localtime()).encode()*3 for i in range(2)], ['5 minutes ago', 'now']))
+    print(next(store.getblobs('now', '2 minutes ago', pull=0), None))
