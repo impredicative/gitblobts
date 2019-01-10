dc58e9048634215ed2aacd87065b1b64cb5ef6be
Ouroboros Chrysopoeia <impredicative>
2019-01-09 19:17:07 -0500

    Add delay for logs to flush

--- gitblobts/__main__.py
+++ gitblobts/__main__.py
@@ -1,11 +1,16 @@
+import time
+
 from gitblobts.config import configure_logging
 from gitblobts.store import Store
 
 configure_logging()
 
 if __name__ == '__main__':
-    import time
-    store = Store('/home/devuser/Documents/blobdumptest', compression=None or 'bz2',
-                  key=None or b'JVGmuw3wRntCc7dcQHJ5q1noUs62ydR0Nw8HpyllKn8=')
-    print(store.addblobs([str(time.localtime()).encode()*3 for i in range(2)], ['5 minutes ago', 'now']))
-    print(next(store.getblobs('now', '2 minutes ago', pull=0), None))
+    try:
+        store = Store('/home/devuser/Documents/blobdumptest', compression=None or 'bz2',
+                      key=None or b'JVGmuw3wRntCc7dcQHJ5q1noUs62ydR0Nw8HpyllKn8=')
+        print(store.addblobs([str(time.localtime()).encode()*3 for i in range(2)], ['5 minutes ago', 'now']))
+        print(next(store.getblobs('now', '2 minutes ago', pull=0), None))
+    except Exception:
+        time.sleep(.01)  # Wait for logs to flush.
+        raise
