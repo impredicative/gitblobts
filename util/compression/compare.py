677c7b02a00fdd8837b49ffafa82bef96596c72b
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-01-10 12:13:21 -0500

    Add decompression times to results

--- util/compression/compare.py
+++ util/compression/compare.py
@@ -11,13 +11,22 @@ _COMPRESSORS = ['bz2', 'gzip', 'lzma', 'zlib']
 def compare_compressors(filetype: str, samples: List[bytes], compressors: List[str] = _COMPRESSORS):
     stats = []
     for compressor in compressors:
-        compress = importlib.import_module(compressor).compress
-        time_start = time.time()
-        ratios = [len(compress(s))/len(s) for s in samples]
-        time_used = time.time() - time_start
-        ratios = [r * 100 for r in ratios]
+        compressor_module = importlib.import_module(compressor)
+
+        compression_time_start = time.time()
+        compressed = [compressor_module.compress(s) for s in samples]
+        compression_time_used = time.time() - compression_time_start
+
+        decompression_time_start = time.time()
+        decompressed = [compressor_module.decompress(s) for s in compressed]
+        decompression_time_used = time.time() - decompression_time_start
+        assert samples == decompressed
+
+        ratios = [100 * len(c) / len(s) for c, s in zip(compressed, samples)]
+
         stat = {
-                'filetype': filetype, 'compressor': compressor, 'total_time': time_used,
+                'filetype': filetype, 'compressor': compressor,
+                'total_compression_time': compression_time_used, 'total_decompression_time': decompression_time_used,
                 'mean': statistics.mean(ratios), 'stdev': statistics.stdev(ratios),
                 'min': min(ratios), 'max': max(ratios),
         }
@@ -40,11 +49,13 @@ if __name__ == '__main__':
 
 """
 Results:
-html: bz2: total_time=0.02 mean=24.57 stdev=1.91 min=21.59 max=26.36
-html: gzip: total_time=0.01 mean=25.43 stdev=1.61 min=22.91 max=26.96
-html: lzma: total_time=0.09 mean=23.25 stdev=1.81 min=20.43 max=24.92
-jpg: bz2: total_time=0.17 mean=98.83 stdev=1.69 min=95.94 max=100.22
-jpg: gzip: total_time=0.04 mean=98.72 stdev=2.04 min=95.22 max=99.97
-jpg: lzma: total_time=0.37 mean=99.00 stdev=1.90 min=95.64 max=100.05
+html: bz2: total_compression_time=0.02 total_decompression_time=0.01 mean=24.57 stdev=1.91 min=21.59 max=26.36
+html: gzip: total_compression_time=0.01 total_decompression_time=0.00 mean=25.43 stdev=1.61 min=22.91 max=26.96
+html: lzma: total_compression_time=0.08 total_decompression_time=0.01 mean=23.25 stdev=1.81 min=20.43 max=24.92
+html: zlib: total_compression_time=0.01 total_decompression_time=0.00 mean=25.54 stdev=1.59 min=23.05 max=27.05
+jpg: bz2: total_compression_time=0.17 total_decompression_time=0.09 mean=98.83 stdev=1.69 min=95.94 max=100.22
+jpg: gzip: total_compression_time=0.04 total_decompression_time=0.01 mean=98.72 stdev=2.04 min=95.22 max=99.97
+jpg: lzma: total_compression_time=0.35 total_decompression_time=0.02 mean=99.00 stdev=1.90 min=95.64 max=100.05
+jpg: zlib: total_compression_time=0.04 total_decompression_time=0.00 mean=98.72 stdev=2.03 min=95.24 max=99.97
 """
 
