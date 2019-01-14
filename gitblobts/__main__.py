aea2863237ed97f5d24354c67468e02f180d83ec
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-01-14 16:12:53 -0500

    Update example

--- gitblobts/__main__.py
+++ gitblobts/__main__.py
@@ -9,8 +9,8 @@ if __name__ == '__main__':
     try:
         store = Store('/home/devuser/Documents/blobdumptest', compression=None or 'bz2',
                       key=None or b'JVGmuw3wRntCc7dcQHJ5q1noUs62ydR0Nw8HpyllKn8=')
-        # print(store.addblobs([b'a2010a', b'c2121c', b'b2020b', b'd4567d'], ['2010', '2121', '2020', '4567']))
-        # print(list(store.getblobs('1900', '8000', pull=1)))
+        # print(store.addblobs([b'a2010a', b'c2121c', b'b2020b', b'd4567d'][0:], ['2010', '2121', '2020', '4567'][0:]))
+        # print(list(store.getblobs('1900', '8000', pull=0)))
     except Exception:
         time.sleep(.01)  # Wait for logs to flush.
         raise
