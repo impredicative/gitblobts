7a6fd9ee4368a5fb98132e9aae94db436cf758ea
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-01-15 00:06:49 -0500

    Use subtests

--- tests/test_requirements.py
+++ tests/test_requirements.py
@@ -5,11 +5,12 @@ import pkg_resources
 
 
 class TestRequirements(unittest.TestCase):
-
     def test_requirements(self):
         """Recursively confirm that requirements are available."""
         # Ref: https://stackoverflow.com/a/45474387/
         requirements = (Path(__file__).parents[1] / 'requirements.in').read_text().strip().split('\n')
         requirements = [r.strip() for r in requirements]
         requirements = [r for r in sorted(requirements) if r and not r.startswith('#')]
-        pkg_resources.require(requirements)
+        for requirement in requirements:
+            with self.subTest(requirement=requirement):
+                pkg_resources.require(requirement)
