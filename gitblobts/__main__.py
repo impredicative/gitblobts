ef55b787afa5a8e51ef85514218116036f450013
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-01-14 21:47:04 -0500

    Change repo dir

--- gitblobts/__main__.py
+++ gitblobts/__main__.py
@@ -7,7 +7,7 @@ configure_logging()
 
 if __name__ == '__main__':
     try:
-        store = Store('/home/devuser/Documents/blobdumptest', compression=None or 'bz2',
+        store = Store('/home/devuser/Documents/blobstoretest', compression=None or 'bz2',
                       key=None or b'JVGmuw3wRntCc7dcQHJ5q1noUs62ydR0Nw8HpyllKn8=')
         # print(store.addblobs([b'a2010a', b'c2121c', b'b2020b', b'd4567d'][0:], ['2010', '2121', '2020', '4567'][0:]))
         # print(list(store.getblobs('1900', '8000', pull=0)))
