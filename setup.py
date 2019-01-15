4b727242a3b82dc22e7142bdf7c854b86a9bae46
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-01-15 01:51:59 -0500

    Move __main__.py to scripts dir

--- setup.py
+++ setup.py
@@ -5,10 +5,7 @@ from setuptools import setup, find_packages
 
 
 def parse_requirements(filename: str) -> List[str]:
-    """Return requirements from requirements file.
-
-    The "MANIFEST.in" file must exist with the line "include requirements.in".
-    """
+    """Return requirements from requirements file."""
     # Ref: https://stackoverflow.com/a/50368460/
     requirements = (Path(__file__).parent / filename).read_text().strip().split('\n')
     requirements = [r.strip() for r in requirements]
@@ -18,13 +15,13 @@ def parse_requirements(filename: str) -> List[str]:
 
 setup(
     name='gitblobts',
-    version='0.0.2',
+    version='0.0.3',
     description='git-backed time-indexed blob storage',
     keywords='git bytes mirroring storage time',
     long_description=Path(__file__).with_name('README.md').read_text().strip(),
     long_description_content_type='text/markdown',
     url='https://github.com/impredicative/gitblobts/',
-    packages=find_packages(exclude=['tests', 'util']),
+    packages=find_packages(exclude=['scripts', 'tests', 'util']),
     install_requires=parse_requirements('requirements.in'),
     python_requires='>=3.7',
     package_data={'gitblobts': ['logging.conf']},
