fd9b78db7b9b082360a00582d958984e92e9f25f
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-01-14 23:58:18 -0500

    Add test to check requirements

---
+++ tests/test_requirements.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+import unittest
+
+import pkg_resources
+
+
+class TestRequirements(unittest.TestCase):
+
+    def test_requirements(self):
+        """Recursively confirm that requirements are available."""
+        # Ref: https://stackoverflow.com/a/45474387/
+        requirements = (Path(__file__).parents[1] / 'requirements.in').read_text().strip().split('\n')
+        requirements = [r.strip() for r in requirements]
+        requirements = [r for r in sorted(requirements) if r and not r.startswith('#')]
+        pkg_resources.require(requirements)
