9de908940c15b2ace08b84d7e4c3f95d3f069b4c
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-01-16 18:57:37 -0500

    Add author and author_email, and bump version to 0.0.5

--- setup.py
+++ setup.py
@@ -15,7 +15,9 @@ def parse_requirements(filename: str) -> List[str]:
 
 setup(
     name='gitblobts',
-    version='0.0.4',
+    author='Ouroboros Chrysopoeia',
+    author_email='impredicative@users.nomail.github.com',
+    version='0.0.5',
     description='git-backed time-indexed blob storage',
     keywords='git bytes mirroring storage time',
     long_description=Path(__file__).with_name('README.md').read_text().strip(),
