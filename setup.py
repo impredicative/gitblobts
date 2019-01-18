39193d2c56ac68dba95e3e3e6df49e1e6fe5c74c
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-01-17 20:18:31 -0500

    Bump version to 0.0.7 due to doc fixes

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     name='gitblobts',
     author='Ouroboros Chrysopoeia',
     author_email='impredicative@users.nomail.github.com',
-    version='0.0.6',
+    version='0.0.7',
     description='git-backed time-indexed blob storage',
     keywords='git bytes mirroring storage time',
     long_description=Path(__file__).with_name('README.md').read_text().strip(),
